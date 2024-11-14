7685ee6e3fb3e7ea9f3088a0063854d06a8ba946
Raphael Oliveira <raphael.foliveira1@gmail.com>
2024-11-13 21:29:52 -0300

    feat: initial implementation

--- main.py
+++ main.py
@@ -1,4 +1,17 @@
-from ply import lex
+from parser import parser
+
 
 if __name__ == "__main__":
-    pass
+
+    # Parse an expression
+    ast = parser.parse(
+        """
+    x = 2 * 3 + 4 * (5 - x)
+
+    if x > 5:
+        print("x is greater than 5")
+    else:
+        print("x is less than or equal to 5")
+    """
+    )
+    print(ast)
