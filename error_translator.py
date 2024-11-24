68d62f86942c87091a7dc2cbf571d7351f0748c7
Manoel Cendon Duran <manoel.duran@hotmail.com>
2024-11-24 18:47:58 -0300

    feat: created error translator

---
+++ error_translator.py
@@ -0,0 +1,10 @@
+class ErrorTranslator:
+    def __init__(self, err: Exception):
+        self.__error = err
+    def read_error(self):
+        print('error', self.__error)
+        if('Invalid' in str(self.__error)):
+            return  'Maluquice eu não entendo, painhô!'
+        if('NoneType' in str(self.__error)):
+            return 'Deu ruim, não entendi nada!!!'
+        return 'Deu ruim, não entendi nada!!!'
